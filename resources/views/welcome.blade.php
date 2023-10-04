ba04727966b6f6fafef3daebd2e764c51da240de
ameen06 <ameename406@gmail.com>
2023-10-04 21:27:17 +0530

    home page slides added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Gyanbharal</title>
 
     @vite(['resources/css/app.css','resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" rel="stylesheet">
+    <style>
+        .splide__slide img {
+            width: 100%;
+            height: auto;
+        }
+    </style>
 </head>
 <body class="antialiased bg-gradient-to-tr from-amber-100 to-amber-50">
     {{-- header --}}
@@ -61,94 +68,109 @@
     </header>
 
     {{-- categories --}}
-    <section class="w-full max-w-2xl mx-auto h-screen pt-24 pb-8 px-4 overflow-auto">
-        <div class="w-full h-full flex justify-center items-center">
-            <div class="w-full grid grid-cols-2 lg:grid-cols-3 gap-6">
-                <a href="{{route('e-a-words')}}" class="w-full h-fit group">
-                    <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-orange-600">
-                        <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+    <section class="w-full max-w-2xl mx-auto pt-24 pb-8 px-4 lg:pb-20">
+        <div class="w-full">
+            <section id="home-banners" class="splide" aria-label="Gyanbharal Banners">
+                <div class="splide__track">
+                      <ul class="splide__list">
+                            @foreach ($slides as $slide)   
+                            <li class="splide__slide">
+                                <img src="https://ik.imagekit.io/k4cixy45r/{{$slide->image_link}}" alt="Slides">
+                            </li>
+                            @endforeach
+                      </ul>
+                </div>
+            </section>
+        </div>
 
-                        <div class="w-full px-4 py-2 mt-4 bg-orange-400 group-hover:bg-orange-600">
-                            <h2 class="text-base font-semibold text-gray-900">A - অ</h2>
-                        </div>
+        <div class="w-full mt-8 grid grid-cols-2 lg:grid-cols-3 gap-6">
+            <a href="{{route('e-a-words')}}" class="w-full h-fit group">
+                <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-orange-600">
+                    <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+
+                    <div class="w-full px-4 py-2 mt-4 bg-orange-400 group-hover:bg-orange-600">
+                        <h2 class="text-base font-semibold text-gray-900">A - অ</h2>
                     </div>
-                </a>
+                </div>
+            </a>
 
-                <a href="{{route('a-e-words')}}" class="w-full h-fit group">
-                    <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-lime-600">
-                        <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <a href="{{route('a-e-words')}}" class="w-full h-fit group">
+                <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-lime-600">
+                    <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
 
-                        <div class="w-full px-4 py-2 mt-4 bg-lime-400 group-hover:bg-lime-600">
-                            <h2 class="text-base font-semibold text-gray-900">অ - A</h2>
-                        </div>
+                    <div class="w-full px-4 py-2 mt-4 bg-lime-400 group-hover:bg-lime-600">
+                        <h2 class="text-base font-semibold text-gray-900">অ - A</h2>
                     </div>
-                </a>
+                </div>
+            </a>
 
-                <a href="{{route('kids-playstream')}}" class="w-full h-fit group">
-                    <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-green-600">
-                        <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <a href="{{route('kids-playstream')}}" class="w-full h-fit group">
+                <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-green-600">
+                    <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
 
-                        <div class="w-full px-4 py-2 mt-4 bg-green-400 group-hover:bg-green-600">
-                            <h2 class="text-base font-semibold text-gray-900">Kids</h2>
-                        </div>
+                    <div class="w-full px-4 py-2 mt-4 bg-green-400 group-hover:bg-green-600">
+                        <h2 class="text-base font-semibold text-gray-900">Kids</h2>
                     </div>
-                </a>
+                </div>
+            </a>
 
-                <a href="{{route('daily-facts')}}" class="w-full h-fit group">
-                    <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-yellow-600">
-                        <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <a href="{{route('daily-facts')}}" class="w-full h-fit group">
+                <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-yellow-600">
+                    <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
 
-                        <div class="w-full px-4 py-2 mt-4 bg-yellow-400 group-hover:bg-yellow-600">
-                            <h2 class="text-base font-semibold text-gray-900">Daily Facts</h2>
-                        </div>
+                    <div class="w-full px-4 py-2 mt-4 bg-yellow-400 group-hover:bg-yellow-600">
+                        <h2 class="text-base font-semibold text-gray-900">Daily Facts</h2>
                     </div>
-                </a>
+                </div>
+            </a>
 
-                <a href="#" class="w-full h-fit group">
-                    <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-rose-600">
-                        <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <a href="#" class="w-full h-fit group">
+                <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-rose-600">
+                    <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
 
-                        <div class="w-full px-4 py-2 mt-4 bg-rose-400 group-hover:bg-rose-600">
-                            <h2 class="text-base font-semibold text-gray-900">Quiz</h2>
-                        </div>
+                    <div class="w-full px-4 py-2 mt-4 bg-rose-400 group-hover:bg-rose-600">
+                        <h2 class="text-base font-semibold text-gray-900">Quiz</h2>
                     </div>
-                </a>
+                </div>
+            </a>
 
-                <a href="{{route('essays.index')}}" class="w-full h-fit group">
-                    <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-cyan-600">
-                        <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <a href="{{route('essays.index')}}" class="w-full h-fit group">
+                <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-cyan-600">
+                    <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
 
-                        <div class="w-full px-4 py-2 mt-4 bg-cyan-400 group-hover:bg-cyan-600">
-                            <h2 class="text-base font-semibold text-gray-900">Essay</h2>
-                        </div>
+                    <div class="w-full px-4 py-2 mt-4 bg-cyan-400 group-hover:bg-cyan-600">
+                        <h2 class="text-base font-semibold text-gray-900">Essay</h2>
                     </div>
-                </a>
+                </div>
+            </a>
 
-                <a href="{{route('phrases.index')}}" class="w-full h-fit group">
-                    <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-purple-600">
-                        <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <a href="{{route('phrases.index')}}" class="w-full h-fit group">
+                <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-purple-600">
+                    <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
 
-                        <div class="w-full px-4 py-2 mt-4 bg-purple-400 group-hover:bg-purple-600">
-                            <h2 class="text-base font-semibold text-gray-900">Phrases</h2>
-                        </div>
+                    <div class="w-full px-4 py-2 mt-4 bg-purple-400 group-hover:bg-purple-600">
+                        <h2 class="text-base font-semibold text-gray-900">Phrases</h2>
                     </div>
-                </a>
+                </div>
+            </a>
 
-                <a href="{{route('grammer.index')}}" class="w-full h-fit group">
-                    <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-sky-300">
-                        <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <a href="{{route('grammer.index')}}" class="w-full h-fit group">
+                <div class="w-full bg-white pt-4 text-center rounded-md shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-sky-300">
+                    <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
 
-                        <div class="w-full px-4 py-2 mt-4 bg-sky-400 group-hover:bg-sky-600">
-                            <h2 class="text-base font-semibold text-gray-900">Grammer</h2>
-                        </div>
+                    <div class="w-full px-4 py-2 mt-4 bg-sky-400 group-hover:bg-sky-600">
+                        <h2 class="text-base font-semibold text-gray-900">Grammer</h2>
                     </div>
-                </a>
-            </div>
+                </div>
+            </a>
         </div>
     </section>
 
     <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js" integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
     <script>
+        new Splide('#home-banners').mount();
+
         $('#mheader-mobile-menu').hide();
 
         $(document).ready(function(){
