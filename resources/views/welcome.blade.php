36b7161903154cd95342beb0aa7fb2a02710255c
ameen06 <ameename406@gmail.com>
2023-10-06 17:07:08 +0530

    features icon update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@ Gyanbharal
 <div class="w-full mt-8 grid grid-cols-2 lg:grid-cols-3 gap-6">
     <a href="{{route('e-a-words')}}" class="w-full h-fit group">
         <div class="w-full bg-white pt-4 text-center rounded-lg shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-orange-600">
-            <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <img src="https://ik.imagekit.io/3bdrc2hss/english-words.png?updatedAt=1696591807797" alt="Letters" class="w-20 mx-auto">
 
             <div class="w-full px-4 py-2 mt-4 bg-orange-400 group-hover:bg-orange-600">
                 <h2 class="text-base font-semibold text-gray-900">A - অ</h2>
@@ -40,7 +40,7 @@ Gyanbharal
 
     <a href="{{route('a-e-words')}}" class="w-full h-fit group">
         <div class="w-full bg-white pt-4 text-center rounded-lg shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-lime-600">
-            <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <img src="https://ik.imagekit.io/3bdrc2hss/assamese-words.png?updatedAt=1696591807860" alt="Letters" class="w-20 mx-auto">
 
             <div class="w-full px-4 py-2 mt-4 bg-lime-400 group-hover:bg-lime-600">
                 <h2 class="text-base font-semibold text-gray-900">অ - A</h2>
@@ -50,7 +50,7 @@ Gyanbharal
 
     <a href="{{route('kids-playstream')}}" class="w-full h-fit group">
         <div class="w-full bg-white pt-4 text-center rounded-lg shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-green-600">
-            <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <img src="https://ik.imagekit.io/3bdrc2hss/kids-page.png?updatedAt=1696591807854" alt="Kids" class="w-20 mx-auto">
 
             <div class="w-full px-4 py-2 mt-4 bg-green-400 group-hover:bg-green-600">
                 <h2 class="text-base font-semibold text-gray-900">Kids</h2>
@@ -60,7 +60,7 @@ Gyanbharal
 
     <a href="{{route('daily-facts')}}" class="w-full h-fit group">
         <div class="w-full bg-white pt-4 text-center rounded-lg shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-yellow-600">
-            <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <img src="https://ik.imagekit.io/3bdrc2hss/daily-facts.png?updatedAt=1696591807804" alt="Daily Facts" class="w-20 mx-auto">
 
             <div class="w-full px-4 py-2 mt-4 bg-yellow-400 group-hover:bg-yellow-600">
                 <h2 class="text-base font-semibold text-gray-900">Daily Facts</h2>
@@ -70,7 +70,7 @@ Gyanbharal
 
     <a href="{{route('quiz')}}" class="w-full h-fit group">
         <div class="w-full bg-white pt-4 text-center rounded-lg shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-rose-600">
-            <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <img src="https://ik.imagekit.io/3bdrc2hss/quiz.png?updatedAt=1696591807692" alt="Quiz" class="w-20 mx-auto">
 
             <div class="w-full px-4 py-2 mt-4 bg-rose-400 group-hover:bg-rose-600">
                 <h2 class="text-base font-semibold text-gray-900">Quiz</h2>
@@ -80,7 +80,7 @@ Gyanbharal
 
     <a href="{{route('essays.index')}}" class="w-full h-fit group">
         <div class="w-full bg-white pt-4 text-center rounded-lg shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-cyan-600">
-            <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <img src="https://ik.imagekit.io/3bdrc2hss/essays.png?updatedAt=1696591807857" alt="Essays" class="w-20 mx-auto">
 
             <div class="w-full px-4 py-2 mt-4 bg-cyan-400 group-hover:bg-cyan-600">
                 <h2 class="text-base font-semibold text-gray-900">Essay</h2>
@@ -90,7 +90,7 @@ Gyanbharal
 
     <a href="{{route('phrases.index')}}" class="w-full h-fit group">
         <div class="w-full bg-white pt-4 text-center rounded-lg shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-purple-600">
-            <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <img src="https://ik.imagekit.io/3bdrc2hss/phrases.png?updatedAt=1696591807801" alt="Phrases" class="w-20 mx-auto">
 
             <div class="w-full px-4 py-2 mt-4 bg-purple-400 group-hover:bg-purple-600">
                 <h2 class="text-base font-semibold text-gray-900">Phrases</h2>
@@ -100,7 +100,7 @@ Gyanbharal
 
     <a href="{{route('grammer.index')}}" class="w-full h-fit group">
         <div class="w-full bg-white pt-4 text-center rounded-lg shadow-2xl shadow-amber-100 overflow-hidden group-hover:shadow-sky-300">
-            <img src="https://img.freepik.com/free-vector/english-alphabets-many-colors_1308-4763.jpg" alt="Letters" class="w-20 mx-auto">
+            <img src="https://ik.imagekit.io/3bdrc2hss/english-grammer.png?updatedAt=1696591807553" alt="Grammer" class="w-20 mx-auto">
 
             <div class="w-full px-4 py-2 mt-4 bg-sky-400 group-hover:bg-sky-600">
                 <h2 class="text-base font-semibold text-gray-900">Grammer</h2>
